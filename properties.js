45b53ecaad8461e8002c57c5c1723d94a6683aef
Alberto Escobar <albertoescobarjr@gmail.com>
2026-02-04 12:25:20 -0600

    feat: agregar nuevas propiedades

--- properties.js
+++ properties.js
@@ -133,6 +133,143 @@ export const properties = [
             status: "Nueva"
         },
         images: ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg", "09.jpg", "10.jpg", "11.jpg", "12.jpg"]
+    },
+    {
+        id: "10",
+        title: "Hacienda Real, Modelo Cipres",
+        price: "L. 2,272,000",
+        type: "Modern Family Residence",
+        mapUrl: "https://maps.app.goo.gl/S5TkgHvNZu5EeihZ6",
+        locationText: "Ver ubicación, Catacamas",
+        features: {
+            beds: 3,
+            baths: 2,
+            garage: 1,
+            status: "En Planos"
+        },
+        images: ["01.png"]
+    },
+    {
+        id: "11",
+        title: "Hacienda Real, Modelo Duranta",
+        price: "L. 2,461,000",
+        type: "Modern Family Residence",
+        mapUrl: "https://maps.app.goo.gl/S5TkgHvNZu5EeihZ6",
+        locationText: "Ver ubicación, Catacamas",
+        features: {
+            beds: 3,
+            baths: 2,
+            garage: 1,
+            status: "En Planos"
+        },
+        images: ["01.png"]
+    },
+
+    {
+        id: "12",
+        title: "Hacienda Real, Modelo Sauce",
+        price: "L. 2,679,000",
+        type: "Modern Family Residence",
+        mapUrl: "https://maps.app.goo.gl/S5TkgHvNZu5EeihZ6",
+        locationText: "Ver ubicación, Catacamas",
+        features: {
+            beds: 3,
+            baths: 2,
+            garage: 2,
+            status: "En Planos"
+        },
+        images: ["01.png"]
+    },
+
+    {
+        id: "13",
+        title: "Hacienda Real, Modelo Sauce 2",
+        price: "L. 2,790,000",
+        type: "Modern Family Residence",
+        mapUrl: "https://maps.app.goo.gl/S5TkgHvNZu5EeihZ6",
+        locationText: "Ver ubicación, Catacamas",
+        features: {
+            beds: 3,
+            baths: 2,
+            garage: 2,
+            status: "En Planos"
+        },
+        images: ["01.png"]
+    },
+    {
+        id: "14",
+        title: "Hacienda Real, Modelo Sauce Plus",
+        price: "L. 3,305,000",
+        type: "Modern Family Residence",
+        mapUrl: "https://maps.app.goo.gl/S5TkgHvNZu5EeihZ6",
+        locationText: "Ver ubicación, Catacamas",
+        features: {
+            beds: 4,
+            baths: 3,
+            garage: 2,
+            status: "En Planos"
+        },
+        images: ["01.png"]
+    },
+    {
+        id: "15",
+        title: "Hacienda Real, Modelo Jaragua (2 Niveles)",
+        price: "L. 3,900,000",
+        type: "Modern Family Residence",
+        mapUrl: "https://maps.app.goo.gl/S5TkgHvNZu5EeihZ6",
+        locationText: "Ver ubicación, Catacamas",
+        features: {
+            beds: 4,
+            baths: 4,
+            garage: 2,
+            status: "En Planos"
+        },
+        images: ["01.png"]
+    },
+    {
+        id: "16",
+        title: "Hacienda Real, Modelo San Agustín (2 Niveles)",
+        price: "L. 2,980,000",
+        type: "Modern Family Residence",
+        mapUrl: "https://maps.app.goo.gl/S5TkgHvNZu5EeihZ6",
+        locationText: "Ver ubicación, Catacamas",
+        features: {
+            beds: 3,
+            baths: 2,
+            garage: 2,
+            status: "En Planos"
+        },
+        images: ["01.png"]
+    },
+    {
+        id: "17",
+        title: "Las Uvas 3, Modelo Valencia",
+        price: "L. 1,784,000",
+        type: "Modern Family Residence",
+        mapUrl: "https://maps.app.goo.gl/WhABTaUKhFWyRBx89",
+        locationText: "Ver ubicación, Catacamas",
+        features: {
+            beds: 2,
+            baths: 1,
+            garage: 1,
+            status: "En Planos"
+        },
+        images: ["01.png"]
+    },
+    {
+        id: "18",
+        title: "Las Uvas 3, Modelo Segovia",
+        price: "L. 1,595,000",
+        type: "Modern Family Residence",
+        mapUrl: "https://maps.app.goo.gl/WhABTaUKhFWyRBx89",
+        locationText: "Ver ubicación, Catacamas",
+        features: {
+            beds: 3,
+            baths: 2,
+            garage: 1,
+            status: "En Planos"
+        },
+        images: ["01.png"]
     }
 
 ];
